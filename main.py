95212f66f712c924ae5eb5d48795b78062abf153
Deennn <abideenlawal70@gmail.com>
2022-05-29 00:15:14 +0100

    done

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
-# Check if two words are anagrams 
-# Example:
-# find_anagrams("hello", "check") --> False
-# find_anagrams("below", "elbow") --> True
-
-
-def find_anagram(word, anagram):
-    # [assignment] Add your code here
-    counter = [0] * 256
-    for i in word:
-        counter[ord(i)] += 1
-    # print(counter)
-    for i in anagram:
-        counter[ord(i)] -= 1
-    # print(counter)
-    return counter.count(0) == 256
-print(find_anagram("hello", "check"))
-print(find_anagram("elbow", "below"))
+from tokenize import String
+ 
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(this, name, age, tracks, score):
+        this.name = str(name)
+        this.age = int(age)
+        this.tracks = tracks
+        this.score = float(score)
+    
+    def change_name(this, new_name):
+        if type(new_name) != str:
+            return "name must be string"
+        this.name = str(new_name)
+ 
+    def change_age(this, new_age):
+        if type(new_age != int):
+            return "age must be an integer"
+        this.age = int(new_age)
+    
+    def add_track(this, new_track):
+        if (type(new_track) != str):
+            return "track must be a string"
+        this.tracks.append(new_track)
+ 
+    def get_score(this):
+        return this.score
+ 
+ 
+ 
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+ 
+# Expected methods
+Bob.change_name("Michael")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
