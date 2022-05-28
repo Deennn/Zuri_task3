cd4ed397f354b6a3ada50cec810556c9ce5079f7
Deennn <abideenlawal70@gmail.com>
2022-05-28 14:19:26 +0100

    done

---
+++ main.py
@@ -0,0 +1,20 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    counter = [0] * 256
+    for i in word:
+        counter[ord(i)] += 1
+    # print(counter)
+    for i in anagram:
+        counter[ord(i)] -= 1
+    # print(counter)
+
+  
+    return counter.count(0) == 256
+print(find_anagram("hello", "check"))
+print(find_anagram("elbow", "below"))
